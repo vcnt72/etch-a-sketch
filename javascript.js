314d3ae95cfc01ab3bdac2cfe0e3446886b5f3a5
vcnt72 <vincentsanjaya72@gmail.com>
2018-11-21 15:59:36 +0700

    add index.html structure a bunch of essential css for the project and the javascript for the DOM

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const theContainer = document.querySelector('#container')
+
+
+for(let i = 0; i < 16;i++){
+  const newDiv = document.createElement('div')
+  newDiv.classList.add('item')
+  theContainer.appendChild(newDiv)
+}
+
+
+
+
+
+
+function theDiv(i){
+  let totalGrid = prompt("How many grid do you want \n n x n")
+  for(let i = 0; i < totalGrid;i++){
+
+  }
+}
